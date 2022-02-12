ccd6e3bdc8bd542804db44dd52b6b4740a82209c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-02-12 13:52:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,33 +11,29 @@ const App = () => {
   const [anegdots, setAnegdots] = useState([]);
   const [anegdot, setAnegdot] = useState('');
   const [anegdotCategory, setAnegdotCategory] = useState('');
+  const [anegdotMessage, setAnegdotMessage] = useState('');
+  const [anegdotCategoryMessage, setAnegdotCategoryMessage] = useState('');
+  const [messageColor, setMessageColor] = useState('');
+  const [inputColor, setInputColor] = useState('');
 
   let modalDisplay = isModalVisible ? 'block' : 'none';
 
-  // useEffect(() => {
-  //   function handleKeyDown() {
-  //     window.addEventListener('keydown', e => {
-  //       if (e.code === 'Escape') {
-  //         setIsModalVisible(false);
-  //       }
-  //     });
-  //   }
+  useEffect(() => {
+    function handleKeyDown() {
+      window.addEventListener('keydown', e => {
+        if (e.code === 'Escape') {
+          setIsModalVisible(false);
+          clearMessages();
+        }
+      });
+    }
 
-  //   handleKeyDown();
-  // }, []);
+    handleKeyDown();
+  }, []);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // create anegdot object
-    // keys: text, category, likes
-    const anegdotObj = {
-      text: anegdot,
-      category: anegdotCategory,
-      likes: 0
-    };
-
+  const addAnegdot = (anegdotObject) => {
     // update state: copy old anegdotes and add new one
-    setAnegdots([...anegdots, anegdotObj]);
+    setAnegdots([...anegdots, anegdotObject]);
 
     // after 500ms close modal and clear the fields
     setTimeout(() => {
@@ -49,7 +45,50 @@ const App = () => {
     }, 500);
   }
 
-  console.log(anegdots);
+  const clearMessages = () => {
+    setAnegdotMessage('');
+    setAnegdotCategoryMessage('');
+    setInputColor('gray');
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // create anegdot object
+    // keys: text, category, likes
+    const anegdotObj = {
+      text: anegdot,
+      category: anegdotCategory,
+      likes: 0
+    };
+
+    // validate form
+    if (!anegdot && !anegdotCategory) {
+      setAnegdotMessage('Fill in this field');
+      setAnegdotCategoryMessage('Fill in this field');
+      setMessageColor('red');
+      setInputColor('red');
+    } 
+    if (!anegdot) {
+      setAnegdotMessage('Fill in this field');
+      setMessageColor('red');
+      setInputColor('red');
+    }
+    if (!anegdotCategory) {
+      setAnegdotCategoryMessage('Fill in this field');
+      setMessageColor('red');
+      setInputColor('red');
+    }
+    
+    if (anegdot && anegdotCategory) {
+      addAnegdot(anegdotObj);
+      clearMessages();
+    }
+  }
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+    clearMessages();
+  }
 
   return (
     <div>
@@ -62,12 +101,16 @@ const App = () => {
       />
       <AddAnegdot
         modalDisplay={modalDisplay}
-        handleModalClose={() => setIsModalVisible(false)}
+        handleModalClose={closeModal}
         handleAnegdotName={anegdot}
         handleAnegdotNameChange={(e) => setAnegdot(e.target.value)}
         handleAnegdotCategory={anegdotCategory}
         handleAnegdotCategoryChange={(e) => setAnegdotCategory(e.target.value)}
         handleSubmit={handleSubmit}
+        anegdotMessage={anegdotMessage}
+        categoryMessage={anegdotCategoryMessage}
+        messageColor={messageColor}
+        inputColor={inputColor}
       />
       <AnegdotList anegdots={anegdots} />
     </div>
