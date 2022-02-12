94f2a4292f111ee21e45230fff2fe45b65c2c80d
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-02-12 13:54:58 -0800

    Update AnegdotList component

--- src/Components/AnegdoteList.component.jsx
+++ src/Components/AnegdoteList.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Anegdot from './Anegdot.component';
+import '../Styles/AnegdotList.styles.css';
 
 const AnegdotList = ({ anegdots }) => {
     return (
-        <section className='anegdot-list'>
+        <section style={{ backgroundColor: anegdots.length > 1 ? 'gray' : '' }} className='anegdot-list'>
             {anegdots.map(anegdot => {
                 return (
                     <Anegdot anegdot={anegdot} />
