991ae433837fda5a60202df7848604b288e3538c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-02-16 10:05:41 -0800

    Update AnegdotList component

--- src/Components/AnegdoteList.component.jsx
+++ src/Components/AnegdoteList.component.jsx
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 import Anegdot from './Anegdot.component';
 import '../Styles/AnegdotList.styles.css';
 
-const AnegdotList = ({ anegdots }) => {
+const AnegdotList = ({ anegdots, onUpvote, }) => {
     return (
-        <section style={{ backgroundColor: anegdots.length > 1 ? 'gray' : '' }} className='anegdot-list'>
+        <section style={{ backgroundColor: anegdots.length > 0 ? 'gray' : '' }} className='anegdot-list'>
             {anegdots.map(anegdot => {
                 return (
-                    <Anegdot anegdot={anegdot} />
+                    <Anegdot
+                        key={anegdot.id}
+                        anegdot={anegdot}
+                        onUpvote={onUpvote}
+                    />
                 );
             })}
         </section>
