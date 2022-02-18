3901eeab553963aa0e04a4fc2fe9f04f674cc0cc
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-02-18 10:07:20 -0800

    Add more tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,9 +4,11 @@ import { render, screen } from '@testing-library/react';
 import Header from '../Components/Header.component';
 import CategoryList from '../Components/CategoryList.component';
 import Search from '../Components/Search.component';
+import AddAnegdot from '../Components/AddAnegdot.component';
+import AnegdotList from '../Components/AnegdoteList.component';
 
 describe('<Header />', () => {
-    test('header component is rendered', () => {
+    test('<Header /> component is rendered', () => {
         render(<Header />);
         const header = screen.getByText('Anegdotes App');
         expect(header).toBeInTheDocument();
@@ -14,7 +16,7 @@ describe('<Header />', () => {
 });
 
 describe('<CategoryList />', () => {
-    test('category list component is rendered', () => {
+    test('<CategoryList /> component is rendered', () => {
         const { container } = render(<CategoryList />);
         const categoryList = container.querySelector('.category-list');
         expect(categoryList).toBeInTheDocument();
@@ -22,9 +24,17 @@ describe('<CategoryList />', () => {
 });
 
 describe('<Search />', () => {
-    test('search component is rendered', () => {
+    test('<Search /> component is rendered', () => {
         const { container } = render(<Search />);
         const search = container.querySelector('.search');
         expect(search).toBeInTheDocument();
+    });
+});
+
+describe('<AddAnegdot />', () => {
+    test('<AddAnegdot /> component is rendered', () => {
+        const { container } = render(<AddAnegdot />);
+        const addAnegdot = container.querySelector('.modal');
+        expect(addAnegdot).toBeInTheDocument();
     });
 });
